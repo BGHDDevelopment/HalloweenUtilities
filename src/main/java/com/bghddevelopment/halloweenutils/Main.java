3ae5fb02648a3d242351de13659d8b3bf30f6964
RyanMoodGAMING <ryan@ryanmood.com>
2022-12-29 17:25:21 +0000

    Add Update Checker

--- src/main/java/com/bghddevelopment/halloweenutils/Main.java
+++ src/main/java/com/bghddevelopment/halloweenutils/Main.java
@@ -5,9 +5,18 @@ import com.bghddevelopment.halloweenutils.events.PumpkinPlace;
 import com.bghddevelopment.halloweenutils.mobs.Horse;
 import com.bghddevelopment.halloweenutils.mobs.MobHeads;
 import com.bghddevelopment.halloweenutils.utils.Color;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -26,6 +35,7 @@ public class Main extends JavaPlugin implements Listener {
         this.registerEvents();
         this.registerCommands();
         plugin = this;
+        updateCheck(Bukkit.getConsoleSender(), true);
     }
 
     public void registerEvents() {
@@ -43,6 +53,60 @@ public class Main extends JavaPlugin implements Listener {
         this.getCommand("batswarm").setExecutor(new BatSwarm());
         this.getCommand("scare").setExecutor(new BlindScare());
 
+    }
+
+    public void updateCheck(CommandSender sender, boolean console) {
+        try {
+            String urlString = "https://updatecheck.bghddevelopment.com";
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String input;
+            StringBuffer response = new StringBuffer();
+            while ((input = reader.readLine()) != null) {
+                response.append(input);
+            }
+            reader.close();
+            JsonObject object = new JsonParser().parse(response.toString()).getAsJsonObject();
+
+            if (object.has("plugins")) {
+                JsonObject plugins = object.get("plugins").getAsJsonObject();
+                JsonObject info = plugins.get("HalloweenUtilities").getAsJsonObject();
+                String version = info.get("version").getAsString();
+                Boolean archived = info.get("archived").getAsBoolean();
+                if (archived) {
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate("&cThis plugin has been marked as 'Archived' by BGHDDevelopment LLC."));
+                    sender.sendMessage(Color.translate("&cThis version will continue to work but will not receive updates or support."));
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate(""));
+                    return;
+                }
+                if (version.equals(getDescription().getVersion())) {
+                    if (console) {
+                        sender.sendMessage(Color.translate("&aHalloweenUtilities is on the latest version."));
+                    }
+                } else {
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate("&cYour HalloweenUtilities version is out of date!"));
+                    sender.sendMessage(Color.translate("&cWe recommend updating ASAP!"));
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate("&cYour Version: &e" + getDescription().getVersion()));
+                    sender.sendMessage(Color.translate("&aNewest Version: &e" + version));
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate(""));
+                }
+            } else {
+                sender.sendMessage(Color.translate("&cWrong response from update API, contact plugin developer!"));
+            }
+        } catch (
+                Exception ex) {
+            sender.sendMessage(Color.translate("&cFailed to get updater check. (" + ex.getMessage() + ")"));
+        }
     }
 
 
